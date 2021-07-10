3ca5bff36dca339503fbe1c3dabf0a31460f850d
Daman Oberoi <daman.oberoi@seo-usa.org>
2021-07-10 17:13:40 +0000

    added registration form

--- web.py
+++ web.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '6c3da44efe0b9fcd6bb377c7b9851041'
 
 @app.route("/")
 @app.route("/home")
@@ -9,6 +12,14 @@ def home():
 @app.route("/about")
 def second_page():
     return render_template('about.html', subtitle='About Daman', text='Daman loves basketball!')
+  
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
     
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
