975f810f3db1100885b01259748a156f25d496fb
Daman Oberoi <daman.oberoi@seo-usa.org>
2021-07-12 06:07:33 +0000

    completed register activity

--- web.py
+++ web.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6c3da44efe0b9fcd6bb377c7b9851041'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
 
 @app.route("/")
 @app.route("/home")
@@ -17,6 +30,9 @@ def second_page():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
